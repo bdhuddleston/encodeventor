c10a2f79076a7e93e63e74f3b43bff9a0c48eefa
Bradley Huddleston <huddpitcher@gmail.com>
2018-03-19 08:13:49 -0500

    Simplified files for video 1 on LAMMPS data files

--- LAMMPS_data_files/random_atoms.py
+++ LAMMPS_data_files/random_atoms.py
@@ -1,36 +1,45 @@
-#
-#
-#
-#
-#
+#-------------------------------------------------------# 
+# Generate LAMMPS data file using random atom positions #
+# 														#
+# 	- Bradley Huddleston, February 27, 2018				#
+#-------------------------------------------------------#
 
 import numpy as np
 
-natoms = 100
-cell_size = 10.0
+# Number of atoms to create
+natoms = 1000
 
+# Size of the system cell in Angstroms
+#	assuming an cubic cell, starting at the origin
+system_size = 20.0
+
+# Generate atom positions
+#	Randomness for amorphous glass
 positions = []
 for i in range(natoms):
-    positions.append(np.random.rand(3)*cell_size)
-    
+	positions.append(np.random.rand(3)*system_size)
+	
+# Write LAMMPS data file
+with open('random.data','w') as fdata:
+	# First line is a comment line 
+	fdata.write('Random atoms - written for EnCodeVentor tutorial\n\n')
+	
+	#--- Header ---#
+	# Specify number of atoms and atom types 
+	fdata.write('{} atoms\n'.format(natoms))
+	fdata.write('{} atom types\n'.format(1))
+	# Specify box dimensions
+	fdata.write('{} {} xlo xhi\n'.format(0.0, system_size))
+	fdata.write('{} {} ylo yhi\n'.format(0.0, system_size))
+	fdata.write('{} {} zlo zhi\n'.format(0.0, system_size))
+	fdata.write('\n')
 
-with open('main_atoms.data','w') as lammpsdata:
+	# Atoms section
+	fdata.write('Atoms\n\n')
 
-    lammpsdata.write('LAMMPS data file written for an EnCodeVentor tutorial\n\n')
-    
-    lammpsdata.write('{} atoms\n'.format(natoms))
-    lammpsdata.write('1 atom types\n')
-    lammpsdata.write('{} {} xlo xhi\n'.format(0.0, cell_size))
-    lammpsdata.write('{} {} ylo yhi\n'.format(0.0, cell_size))
-    lammpsdata.write('{} {} zlo zhi\n'.format(0.0, cell_size))
-    lammpsdata.write('\n')
-    
-    lammpsdata.write('Atoms\n\n')
-    for i, pos in enumerate(positions):
-    
-        lammpsdata.write('\t{} {} {} {} {}\n'.format(i+1,1,*pos))
-        
-    # bonds and such below
-    
-    
-
+	# Write each position 
+	for i,pos in enumerate(positions):
+		fdata.write('{} 1 {} {} {}\n'.format(i+1,*pos))
+		
+	# If you have bonds and angles, further sections below
+	
